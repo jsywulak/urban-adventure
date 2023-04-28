e6ba30cc36c39f6aa3b3c81ca1323216113cdfd2
jonny sywulak <jonathan.sywulak@gmail.com>
2023-04-28 17:40:38 +0100

    add cdk code for creating eks cluster

---
+++ infra/bin/infra.ts
@@ -0,0 +1,28 @@
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+
+const app = new cdk.App();
+const account = '207188242297';
+const region = 'eu-west-2';
+
+const addOns: Array<blueprints.ClusterAddOn> = [
+    new blueprints.addons.ArgoCDAddOn(),
+    new blueprints.addons.CalicoOperatorAddOn(),
+    new blueprints.addons.MetricsServerAddOn(),
+    new blueprints.addons.ClusterAutoScalerAddOn(),
+    new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+    new blueprints.addons.VpcCniAddOn(),
+    new blueprints.addons.CoreDnsAddOn(),
+    new blueprints.addons.KubeProxyAddOn(),
+    new blueprints.addons.IstioBaseAddOn(),
+    new blueprints.addons.IstioControlPlaneAddOn(),
+    new blueprints.addons.KNativeOperator()
+  ];
+
+const stack = blueprints.EksBlueprint.builder()
+    .account(account)
+    .region(region)
+    .addOns(...addOns)
+    .useDefaultSecretEncryption(false) // set to false to turn secret encryption off (non-production/demo cases)
+    .build(app, 'eks-blueprint');
